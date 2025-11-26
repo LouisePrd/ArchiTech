e6df070f4ab56853738edacba0b861693a4595a1
Nyxouu <justineperso03@gmail.com>
2025-11-26 17:30:46 +0100

    ajouts new icons

--- app/component/header.js
+++ app/component/header.js
@@ -4,7 +4,7 @@ export default function Header(){
     return(
     <div id={headerstyles.header}>
         <img id={headerstyles.logo} src="/logo.svg" alt="Logo du site"/>
-        <a href="/signin"><img src="/profil.png" alt="User Profil Icon"/></a>
+        <a href="/signin"><img src="/user.svg" alt="User Profil Icon"/></a>
         
     </div>
     )
